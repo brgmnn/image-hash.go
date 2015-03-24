32bf6ea102b88aed2cce2c2fc4f35fb0ee2ac7e6
Daniel Bergmann <dan.arne.bergmann@gmail.com>
2015-03-24 19:26:39 +0000

    Clamped bitdepth and size and expanded on the flags descriptions.

--- image-hash.go
+++ image-hash.go
@@ -18,15 +18,28 @@ import (
 	"github.com/nfnt/resize"
 )
 
+/*		--- clamp() ---
+ * Clamps an integer to a minimum of 'lo' or a maximum of 'hi'. */
+func clamp(num int, lo int, hi int) int {
+	if num > hi {
+		return hi
+	} else if num < lo {
+		return lo
+	}
+
+	return num
+}
+
 func main() {
-	bd := flag.Int("bitdepth", 5, "The number of bits to rescale each pixel to.")
-	wd := flag.Int("size", 4, "The width.")
-	v := flag.Bool("v", false, "Verbose output.")
+	bd := flag.Int("bitdepth", 5, "The number of bits to rescale each pixel "+
+		"to. Must be between 1 and 16.")
+	wd := flag.Int("size", 4, "What 'size' to rescale images to.")
+	v := flag.Bool("v", false, "Verbose output. Print image paths along with hashes.")
 
 	flag.Parse()
 
-	bitdepth := (uint16)(*bd)
-	width := (uint)(*wd)
+	bitdepth := (uint16)(clamp(*bd, 1, 16))
+	width := (uint)(clamp(*wd, 1, 200))
 	verbose := (bool)(*v)
 
 	if len(flag.Args()) < 1 {
@@ -62,7 +75,7 @@ func main() {
 		for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 			for x := bounds.Min.X; x < bounds.Max.X; x++ {
 				g, _, _, _ := color.GrayModel.Convert(imgstd.At(x, y)).RGBA()
-				g >>= 16 - (uint)(bitdepth)
+				g >>= 16 - bitdepth
 
 				binary.Write(buf, binary.LittleEndian, g)
 			}
